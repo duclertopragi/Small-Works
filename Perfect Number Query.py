de49c81d156a5891ca6787cf543f18c7c32e7486
dkn <dogukan>
2023-03-20 12:23:34 +0300

    First

---
+++ Perfect Number Query.py
@@ -0,0 +1,18 @@
+# List perfect number(s) that in given range
+
+print("""
+***** This is a perfect number query. *****\n
+You need to determine 'starting' and 'ending' of the range in order to run the query.
+""")
+
+nb1 = int(input("Enter the starting number of the range: ")) 
+nb2 = int(input("Enter the ending number of the range: "))
+
+for n in range (nb1, nb2+1):
+    smm = 0
+    for i in range(1,n):
+        if n % i == 0:
+          smm += i
+
+    if n == smm:
+        print(n)
