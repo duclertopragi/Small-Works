673de818e596bc7ed182aacc1e3ee032262a8c93
dkn <dogukan>
2023-04-19 11:15:27 +0300

    Revised

--- Perfect Number Query.py
+++ Perfect Number Query.py
@@ -5,14 +5,28 @@ print("""
 You need to input 'starting' and 'ending' of the range in order to run the query.
 """)
 
-nb1 = int(input("Enter the starting number of the range: ")) 
-nb2 = int(input("Enter the ending number of the range: "))
-
-for n in range (nb1, nb2+1):
-    smm = 0
-    for i in range(1,n):
-        if n % i == 0:
-          smm += i
-
-    if n == smm:
-        print(n)
+while True:
+          
+    start = int(input("Please indicate the lowest figure in your range: "))
+    end = int(input("Please indicate the highest figure in your range: "))
+    
+    if start < 0 or end < 0:
+        print("You typed in incorrect figure, please input positive integer")
+        continue
+    
+    perfect_numbers = []
+    
+    for num in range(start, end+1):
+        divisors = []
+        for i in range(1, num):
+            if num % i == 0:
+                divisors.append(i)
+        if sum(divisors) == num:
+            perfect_numbers.append(num)
+    
+    if perfect_numbers:
+        print("Perfect numbers between", start, "and", end, "are:", perfect_numbers)
+    else:
+        print("There are no perfect numbers between", start, "and", end)
+        
+    break
