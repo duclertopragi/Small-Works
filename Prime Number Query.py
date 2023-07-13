445450c904cd4a237af3257fb79e09c655a0ef86
Doğukan Üçlertoprağı <111128960+duclertopragi@users.noreply.github.com>
2023-07-13 14:19:41 +0300

    Prime Number Query.py
    Iteration problem solved

--- Prime Number Query.py
+++ Prime Number Query.py
@@ -14,11 +14,12 @@ while True:
     if num2 < num1:
         print("You have entered numbers incorrectly")
         continue
+    else:
     
-for j in range(num1, num2+1):
-    if j > 1:
-        for i in range(2, int(j/2)+1): # Attention! Attention! This is a sharp expression: "int(j/2)+1". There is no need to look for a divider after the half the number cos there can't be any more divider after the half of it...
-            if (j % i == 0):
-                break
-        else:
-            print(j, "is a prime number")
+               for j in range(num1, num2+1):
+                   if j > 1:
+                       for i in range(2, int(j/2)+1): # Attention! Attention! This is a sharp expression: "int(j/2)+1". There is no need to look for a divider after the half the number cos there can't be any more divider after the half of it...
+                           if (j % i == 0):
+                               break
+                       else:
+                           print(j, "is a prime number")
